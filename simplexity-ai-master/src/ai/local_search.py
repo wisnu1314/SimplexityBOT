d8e75e4ed7cd83e51e7f7b39c5934f444bd74f29
anakpindahan <akeylanaufal@gmail.com>
2021-10-15 15:31:29 +0700

    add class base att and func

--- simplexity-ai-master/src/ai/local_search.py
+++ simplexity-ai-master/src/ai/local_search.py
@@ -6,14 +6,62 @@ from src.model import State
 
 from typing import Tuple, List
 
-
+def piecespec(row, col):
+    return board[row][col].piece
 class LocalSearch:
     def __init__(self):
-        pass
+        self.streak_candidates_self = []
+        self.streak_candidates_enemy = []
+
+    def neighbors(self, state: State, row: int, column: int) -> List[List[Tuple[int, int]]]:
+        neighbors = []
+        batasKiri = max(0, column - 3)
+        batasKanan = min(6, column + 3)
+        batasAtas = min(5, row + 3)
+        batasBawah = max(0, row - 3)
+        for i in range(batasKiri, batasKanan - 2):
+            neighbors.append([(row, j) for j in range(i,i+4)])
+            neighbors.append([(batasAtas - (j-i), j) for j in range(i, i+4)])
+        for i in range(batasBawah, batasAtas - 2):
+            neighbors.append([(j, column) for j in range(i,i+4)])
+            neighbors.append([(j, batasKanan - (j-i)) for j in range(i, i+4)])
+        return neighbors
+
+    def is_diagonal(self, point1,point2):
+        return ((point1.x + 1 == point2.x and point1.y + 1 == point2.y) or (point1.x - 1 == point2.x and point1.y + 1 == point2.y) or (point1.x + 1 == point2.x and point1.y - 1 == point2.y) or (point1.x - 1 == point2.x and point1.y - 1 == point2.y))
+    
+    def is_check(self, point1,point2,point3):
+        Cond_diagonal = False
+        Cond_vertikal = False
+        Cond_horizontal = False
+        if((point1.x - point2.y == 0) and abs(point1.x - point1.y == 1) and (point3.y - point1.y == 0 and abs(point3.x - point1.x == 1)):
+            Cond_horizontal = True
+
+        if((point1.x - point2.x == 0 and abs(point1.y - point2.y == 1) and (point3.x - point1.x == 0 and abs(point3.y - point1.y == 1)):
+            Cond_vertikal = True
+            
+        if(isDiagonal(point1, point2, point3))
+            Cond_diagonal = True
+            
+        return (Cond_horizontal or Cond_vertikal or Cond_horizontal)    
+    
+
+    def is_streak(self, point1: Tuple[int, int], point2: Tuple[int, int], point3: Tuple[int, int], point4: Tuple[int, int]):
+        if(point1[0] == point2[0] and point2[0] == point3[0] and point3[0] == point4[0] and point1[1] == point2[1] - 1
+        and point2[1] == point3[1] - 1 and point3[1] == point4[1] - 1):
+            return True
+        elif(point1[1] == point2[1] and point2[1] == point3[1] and point3[1] == point4[1] and point1[0] == point2[0] - 1
+        and point2[0] == point3[0] - 1 and point3[0] == point4[0] - 1):
+            return True
+        elif()
+    
 
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
 
         best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
 
-        return None
+        return best_movement
+
+            
+            
--- simplexity-ai-master/src/mechanic/game.py
+++ simplexity-ai-master/src/mechanic/game.py
@@ -1,7 +1,7 @@
 import pickle
 from time import time
 
-from src.ai import Minimax
+from src.ai import Minimax, LocalSearch
 from src.model import Board, Player, State, Config
 from src.constant import ShapeConstant, GameConstant, Path
 from src.utility import is_out, is_win, is_full, place
@@ -56,7 +56,7 @@ class Game:
         elif self.config.game_type == GameConstant.PVB:
             if not self.config.is_dump:
                 # You can change model used here
-                model = Minimax()
+                model = LocalSearch()
             else:
                 # Don't change this
                 model = pickle.load(open(Path.PVB, "rb"))
